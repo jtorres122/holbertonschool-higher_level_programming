0b981a49486d3376aadac40b9f5eb244f43b467e
Jose Torres <jose.torres122@outlook.com>
2021-09-08 20:35:12 +0000

    check last digit of number

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,6 +6,9 @@ ld = abs(number) % 10
 string1 = "Last digit of "
 string2 = "is less than 6 and not 0"
 
+if number < 0:
+    ld = ld * -1
+
 if ld == 0:
     print("{:s}{:d} is {:d} and is 0".format(string1, number, ld))
 elif ld < 6 and ld != 0:
