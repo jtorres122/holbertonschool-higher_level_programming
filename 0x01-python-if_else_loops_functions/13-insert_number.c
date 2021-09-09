b6b197045bf318881ae32795dad0d58f3598d134
Jose Torres <jose.torres122@outlook.com>
2021-09-09 02:36:25 +0000

    Header file and first commit of task 13

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+ * insert_node - function
+ * @head: parameter
+ * @number: parameter
+ * Return: the address of the new node, or NULL if it failed
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	return (NULL);
+}
