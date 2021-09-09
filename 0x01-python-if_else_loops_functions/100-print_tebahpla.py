94e5743ffb32ee4b04c1f8a75cef96390ac134c5
Jose Torres <jose.torres122@outlook.com>
2021-09-09 00:54:20 +0000

    Print alphabet in reverse order alternating lowercase and uppercase

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+for alpha in range(122, 96, -1):
+    if alpha % 2 != 0:
+        alpha = alpha - 32
+        print("{:c}".format(alpha), end='')
+    else:
+        print("{:c}".format(alpha), end='')
