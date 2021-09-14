e0afde68c8ae83279c7999ebac0275e2dab003ce
Jose Torres <jose.torres122@outlook.com>
2021-09-14 19:20:03 +0000

    Print a matrix of integers

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,8 +3,8 @@
 
 def print_matrix_integer(matrix=[[]]):
 
-    if matrix is None:
-        return(None)
+    if len(matrix) <= 0:
+        return
 
     for i in matrix:
         print(' '.join(format(row)for row in i))
