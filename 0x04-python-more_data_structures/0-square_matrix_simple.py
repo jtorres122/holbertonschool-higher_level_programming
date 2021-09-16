d9ddb88a0e1338622e43d65a38d34f4573171719
Jose Torres <jose.torres122@outlook.com>
2021-09-16 22:10:48 +0000

    Be the first commit with README and Task 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -4,10 +4,11 @@
 def square_matrix_simple(matrix=[]):
 
     new_matrix = []
-
-    for i in matrix:
-        for x in i:
-            y = x ** 2
-            new_matrix.append(y)
+    for row in matrix:
+        new = []
+        for col in row:
+            y = col ** 2
+            new.append(y)
+        new_matrix.append(new)
 
     return (new_matrix)
