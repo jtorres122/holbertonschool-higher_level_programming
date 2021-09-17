7e1ca44c5411a40d818cdc1f58c77780d1b38951
Jose Torres <jose.torres122@outlook.com>
2021-09-17 01:16:30 +0000

    Replace or add key/value in a dictionary

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+def update_dictionary(a_dictionary, key, value):
+
+    if key not in a_dictionary:
+        a_dictionary.update({key: value})
+        return(a_dictionary)
+
+    if key in a_dictionary:
+        a_dictionary[key] = value
+        return(a_dictionary)
