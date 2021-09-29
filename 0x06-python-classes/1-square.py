87deefcb7b096c4378558511eed7ab48672a6d4f
Jose Torres <jose.torres122@outlook.com>
2021-09-29 02:24:23 +0000

    Contain a class that defines a square with private instance attribute size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+'''This module contains a class with one attribute'''
+
+
+class Square:
+    '''This class contains private instance attribute size for the square'''
+    def __init__(self, size):
+        self.__size = size
