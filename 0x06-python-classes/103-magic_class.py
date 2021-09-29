0573f82c1f7b08bc8368d32d00cf1268577de4f9
Jose Torres <jose.torres122@outlook.com>
2021-09-29 04:17:59 +0000

    Follow precisely instructed Python bytecode with a class

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -13,8 +13,8 @@ class MagicClass:
             raise TypeError("radius must be a number")
         self.__radius = radius
 
-    def area(self, radius):
+    def area(self):
         return (self.__radius ** 2 * math.pi)
 
-    def circumference(self, radius):
+    def circumference(self):
         return (2 * math.pi * self.__radius)
