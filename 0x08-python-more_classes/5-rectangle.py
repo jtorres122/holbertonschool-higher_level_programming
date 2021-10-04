9f600a090aa1db54de74edcb92022b0a181c0b1d
Jose Torres <jose.torres122@outlook.com>
2021-10-04 22:16:21 +0000

    Rename the directory to it's appropriate name and push all first attempts of Task 0 through 7

---
+++ 0x08-python-more_classes/5-rectangle.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python3
+'''This module contains class Rectangle'''
+
+
+class Rectangle:
+    '''class Rectangle with attributes width and height defined and set'''
+    def __init__(self, width=0, height=0):
+        '''comment'''
+        self.__height = height
+        self.__width = width
+
+    @property
+    def width(self):
+        '''comment'''
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        '''comment'''
+        self.__width = value
+        if isinstance(value, int) is not True:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+
+    @property
+    def height(self):
+        '''comment'''
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        '''comment'''
+        self.__height = value
+        if isinstance(value, int) is not True:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+
+    def area(self):
+        '''comment'''
+        area = self.__width * self.__height
+        return area
+
+    def perimeter(self):
+        '''comment'''
+        if self.__width == 0 or self.__height == 0:
+            perimeter = 0
+        else:
+            perimeter = (2 * self.__width) + (2 * self.__height)
+        return perimeter
+
+    def __str__(self):
+        '''comment'''
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        else:
+            rectangle = ""
+            for row in range(self.__height):
+                for col in range(self.__width):
+                     rectangle += "#"
+                rectangle += "\n"
+            return rectangle[:-1]
+
+    def __repr__(self):
+        '''comment'''
+        return f'Rectangle({self.__width}, {self.__height})'
+
+    def __del__(self):
+        '''comment'''
+        print("Bye rectangle...")
