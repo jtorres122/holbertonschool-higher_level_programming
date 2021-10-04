a879f53fb8b66bc19180ad9c0b6ae0fb46ab304b
Jose Torres <jose.torres122@outlook.com>
2021-10-04 22:20:48 +0000

    Attempt to fix two missing code checkers

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -6,8 +6,8 @@ class Rectangle:
     '''class Rectangle with attributes width and height defined and set'''
     def __init__(self, width=0, height=0):
         '''comment'''
-        self.__height = height
         self.__width = width
+        self.__height = height
 
     @property
     def width(self):
