0ecba9b23b5817a77f41dc982897e128dde42488
Jose Torres <jose.torres122@outlook.com>
2021-10-04 22:07:19 +0000

    Push dir README and task 0

---
+++ 0x08-python_more_classes/0-rectangle.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+'''This module contains an empty class named Rectangle'''
+
+
+class Rectangle:
+    '''This is an empty class'''
---
+++ 0x08-python_more_classes/README.md
@@ -0,0 +1,1 @@
+<h1 align="center">Python - More Classes and Objects</h1>
