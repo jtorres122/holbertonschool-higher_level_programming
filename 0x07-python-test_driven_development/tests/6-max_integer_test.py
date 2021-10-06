dce50144316603e8ee6856292bda8e4e1825c5b9
Jose Torres <jose.torres122@outlook.com>
2021-10-06 03:22:39 +0000

    Corrected parameter

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -13,28 +13,28 @@ class max_integer_test(unittest.TestCase):
     def test_max(self):
         '''comment'''
         arr = [1, 2, 3, 4]
-        self.assertEqual(max_integer(l), 4)
+        self.assertEqual(max_integer(arr), 4)
 
         arr = [0, 23, 45, 55]
-        self.assertEqual(max_integer(l), 55)
+        self.assertEqual(max_integer(arr), 55)
 
         arr = [None]
-        self.assertEqual(max_integer(l), None)
+        self.assertEqual(max_integer(arr), None)
 
         arr = []
-        self.assertEqual(max_integer(l), None)
+        self.assertEqual(max_integer(arr), None)
 
         arr = [7, 1, 2, 3]
-        self.assertEqual(max_integer(l), 7)
+        self.assertEqual(max_integer(arr), 7)
 
         arr = [-1, -2, -3, -5]
-        self.assertEqual(max_integer(l), -1)
+        self.assertEqual(max_integer(arr), -1)
 
         arr = [100]
-        self.assertEqual(max_integer(l), 100)
+        self.assertEqual(max_integer(arr), 100)
 
         arr = [10, 20, 30, 40, 50, 102, 60, 75, 85, 95, 101]
-        self.assertEqual(max_integer(l), 102)
+        self.assertEqual(max_integer(arr), 102)
 
 
 if __name__ == "__main__":
