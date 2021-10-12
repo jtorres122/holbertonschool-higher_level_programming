be5d613628eff9f4a8f56426f5df2fb7888f5813
Jose Torres <jose.torres122@outlook.com>
2021-10-12 23:42:50 +0000

    Add method area to BaseGeometry class that raises an exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''Module contains method area'''
+
+
+class BaseGeometry:
+    '''Class contains method area'''
+
+    def area(self):
+        '''Method raises an exception'''
+        raise Exception("area() is not implemented")
