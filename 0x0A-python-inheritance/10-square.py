d91e030dbce1ba50b5f569566bcc5a57298e69b5
Jose Torres <jose.torres122@outlook.com>
2021-10-13 01:06:22 +0000

    Be a module that contains child class Square that inherits from Rectangle

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+'''Module imports and contains classes BaseGeometry, Rectangle and Square'''
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    '''Child class of Rectangle'''
+
+    def __init__(self, size):
+        '''Method inits instance'''
+        super().integer_validator("size", size)
+        self.__size = size
+        super().__init__(self.__size, self.__size)
+
+    def area(self):
+        '''Method calculates the area of a square'''
+        area = self.__size * self.__size
+        return (area)
