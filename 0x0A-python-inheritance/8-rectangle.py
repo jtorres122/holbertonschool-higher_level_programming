2bcb984d591b6055e95aa91faf2d6250c437a291
Jose Torres <jose.torres122@outlook.com>
2021-10-13 00:40:37 +0000

    Be a module that contains child class Rectangle

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''Module imports and contains classes BaseGeometry and Rectangle'''
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    '''Class is a child of BaseGeometry'''
+
+    def __init__(self, width, height):
+        '''Method inits instances'''
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
+        self.__width = width
+        self.__height = height
