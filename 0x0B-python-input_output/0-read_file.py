0a8f5112adaa717fd9f0a0a16fa1e647613003bd
Jose Torres <jose.torres122@outlook.com>
2021-10-15 06:37:55 +0000

    Add a missing whitespace after mode specification

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,5 +5,5 @@
 def read_file(filename=""):
     '''Function reads a text file and prints to stdout'''
 
-    with open(filename, mode="r",encoding="utf-8") as f:
+    with open(filename, mode="r", encoding="utf-8") as f:
         print(f.read(), end='')
