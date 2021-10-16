e75e8a321cd9d4bdce8e922dfb78135cb026d109
Jose Torres <jose.torres122@outlook.com>
2021-10-16 09:16:07 +0000

    Be a function that writes a string to a txt file and returns the len of text

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''Module contains a function that writes to a txt file'''
+
+def write_file(filename="", text=""):
+    '''
+    Function writes a str to a txt file and returns
+    the number of characters written
+    '''
+
+    with open(filename, "w") as f:
+        f.write(text)
+
+    return (len(text))
