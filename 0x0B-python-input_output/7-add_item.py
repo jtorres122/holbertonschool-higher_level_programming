2bf43525fee46737cb1f5cc7f92f47e9426bfc85
Julian <jose.torres122@outlook.com>
2021-11-01 09:08:35 -0400

    Update save_to_json_file function to make add_item work

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
 '''Module contains function that adds args to a list and save to file'''
 import sys
+import os.path
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-my_list = []
+if (os.path.isfile("add_item.json"))
+    my_list = load_from_json_file("add_item.json")
+else:
+    my_list = []
+
 for i in range(1, len(sys.argv)):
     my_list.append(sys.argv[i])
 
