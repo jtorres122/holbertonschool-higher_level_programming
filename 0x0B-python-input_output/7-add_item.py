4a9a6c0c6714797bd1c270e98c45081c65bd4332
Julian <jose.torres122@outlook.com>
2021-11-01 09:09:40 -0400

    pys unexpected indentation on line 9

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,7 +5,7 @@ import os.path
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-if (os.path.isfile("add_item.json"))
+if (os.path.isfile("add_item.json")):
     my_list = load_from_json_file("add_item.json")
 else:
     my_list = []
