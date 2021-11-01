e1600aeb9b24766351b98371a00a7ff84753fdc5
Julian <jose.torres122@outlook.com>
2021-10-31 20:43:40 -0400

    pys compliance

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-import sys
 '''Module contains function that adds args to a list and save to file'''
+import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
