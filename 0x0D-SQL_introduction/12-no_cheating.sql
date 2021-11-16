cb26e681310475c5820b62b96d3f12226b7699eb
Julian <jose.torres122@outlook.com>
2021-11-16 18:52:49 -0400

    Script updates Bob's score

--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -1,3 +1,2 @@
 -- Script updates the score of Bob to 10
-SELECT name FROM second_table WHERE name='Bob';
-UPDATE second_table SET score = 10;
+SELECT name FROM second_table WHERE name='Bob' UPDATE second_table REPLACE(score, 10);
