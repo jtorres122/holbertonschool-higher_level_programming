aad4388c6f5badf0b6fb0894d088f37fb2d8c692
Julian <jose.torres122@outlook.com>
2021-11-16 17:30:02 -0400

    What the hell is this syntax bruh

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,4 +1,2 @@
 -- Script creates a table in the current database
-CREATE TABLE IF NOT EXISTS first_table (
-    description INT VARCHAR(256)
-);
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
