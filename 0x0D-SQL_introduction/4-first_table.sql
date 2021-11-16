4c2483948597a2a5f3e84a43b1877ba8dcf722c4
Julian <jose.torres122@outlook.com>
2021-11-16 17:27:42 -0400

    Add description

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,5 +1,4 @@
 -- Script creates a table in the current database
-CREATE TABLE IF NOT EXISTS first_table(
-    id INT
-    name VARCHAR(256)
+CREATE TABLE IF NOT EXISTS first_table (
+    description INT VARCHAR(256)
 );
