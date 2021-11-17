4e5e783594333645f53a0f779e14b9a4666cf4b2
Julian <jose.torres122@outlook.com>
2021-11-17 15:57:19 -0400

    Push a script that creates a user in MySQL server

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- Script creates server user
-CREATE USER IF NOT EXISTS user_0d_1 IDENTIFIED BY user_0d_1_pwd;
-GRANT ALL PRIVILEGES TO user_0d_1;
+CREATE USER user_0d_1@localhost IDENTIFIED BY user_0d_1_pwd;
+GRANT ALL PRIVILEGES ON *.* TO user_0d_1@localhost;
