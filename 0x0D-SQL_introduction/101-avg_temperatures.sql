476dd9fe9554c6401af6029856b582e5b75c9ca1
Julian <jose.torres122@outlook.com>
2021-11-18 17:36:06 -0400

    FIx syntax order

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,4 +1,4 @@
 -- Script displays the avg temperature by city ordered by temp
 SELECT city, AVG(value) AS avg_temp FROM temperatures
-ORDER BY avg_temp DESC
-GROUP BY city;
+GROUP BY city
+ORDER BY avg_temp DESC;
