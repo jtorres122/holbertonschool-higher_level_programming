222a7b1afd91bbfeabeefad37ce6c786b127791c
Julian <jose.torres122@outlook.com>
2021-11-18 17:55:01 -0400

    Be a script that display top 3 cities' temp

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,5 @@
+-- Script displays top 3 cities temp during July and August ordered by temp
+SELECT city, AVG(value) AS avg_temp FROM temperatures
+WHERE month=7 or month=8
+GROUP BY city
+ORDER BY avg_temp DESC LIMIT 3;
