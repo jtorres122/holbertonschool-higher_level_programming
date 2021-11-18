907657e7eba7d79a3f4c2285b53e43fee6f21346
Julian <jose.torres122@outlook.com>
2021-11-18 17:35:20 -0400

    Added group by city

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,3 +1,4 @@
 -- Script displays the avg temperature by city ordered by temp
 SELECT city, AVG(value) AS avg_temp FROM temperatures
-ORDER BY avg_temp DESC;
+ORDER BY avg_temp DESC
+GROUP BY city;
