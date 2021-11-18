523f50c5558bc6911e354ee18318c14850876e29
Julian <jose.torres122@outlook.com>
2021-11-17 23:05:22 -0400

    Be the first long table I create with different constraints

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,9 @@
+-- Script creates database and a table with specific long description
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities (
+    id INT NOT NULL UNIQUE AUTO_INCREMENT PRIMARY KEY,
+    state_id INT NOT NULL FOREIGN KEY,
+    name VARCHAR(256) NOT NULL,
+    REFERENCES states(id)
+);
