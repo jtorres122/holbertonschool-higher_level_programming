667a0fa5cc5b9549dda19aa81c295199cd2e12bf
Julian <jose.torres122@outlook.com>
2021-11-18 10:40:06 -0400

    I was naming the columns incorrectly

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,5 @@
 -- Script lists all genres from a database and displays the number of shows linked to each
-SELECT tv_genres.name, COUNT(*) AS show_count FROM tv_genres
+SELECT tv_genres.name AS genre, COUNT(*) AS number_of_shows FROM tv_genres
 INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 GROUP BY tv_genres.name
 ORDER BY show_count DESC;
