85d4da99d4d5b62d76570ccd42e34bc62471412b
Julian <jose.torres122@outlook.com>
2021-11-17 22:57:06 -0400

    Using the database after creating

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,3 +1,4 @@
 -- Script creates database and a table with a specific description
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states IN hbtn_0d_usa (id INT NOT NULL UNIQUE AUTO_INCREMENT, name VARCHAR(256) NOT NULL, PRIMARY KEY (id));
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (id INT NOT NULL UNIQUE AUTO_INCREMENT PRIMARY KEY, name VARCHAR(256) NOT NULL);
