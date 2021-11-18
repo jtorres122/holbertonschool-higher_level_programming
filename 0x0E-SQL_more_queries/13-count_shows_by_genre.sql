673a6daa7ae690db37a9efe106fa71c129fa1392
Julian <jose.torres122@outlook.com>
2021-11-17 23:55:56 -0400

    Gotta love it when it's not a typo

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,6 @@
 -- Script lists all genres from a database and displays the number of shows linked to each
-SELECT tv_genres.name, COUNT(tv_show_genres.show_id) AS show_count FROM tv_genres
+SELECT tv_genres.name, COUNT(tv_show_genres.show_id) AS show_count
+FROM tv_genres
 INNER JOIN tv_show_genres ON tv_genres.id=tv_show_genres.genre_id
 GROUP BY tv_genres.name
 ORDER BY show_count DESC;
