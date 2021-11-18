e7da5e21c6db359daab0b49e689e8e29c0bc9ddb
Julian <jose.torres122@outlook.com>
2021-11-17 23:33:00 -0400

    Typo on line 3 with the show

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,4 +1,4 @@
 -- Script lists all shows contained in a database that have at least on genre linked
 SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
-INNER JOIN tv_show_genres ON tv_shows.id=tv_shows_genres.show_id
+INNER JOIN tv_show_genres ON tv_shows.id=tv_show_genres.show_id
 ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
