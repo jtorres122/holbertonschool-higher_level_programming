57b9cf3d0078f86d6730b1f6a6cd05576ade592b
Julian <jose.torres122@outlook.com>
2021-11-17 22:54:45 -0400

    Moved primary key location

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,3 +1,3 @@
 -- Script creates database and a table with a specific description
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states IN hbtn_0d_usa (id INT NOT NULL UNIQUE AUTO_INCREMENT PRIMARY KEY, name VARCHAR(256) NOT NULL);
+CREATE TABLE IF NOT EXISTS states IN hbtn_0d_usa (id INT NOT NULL UNIQUE AUTO_INCREMENT, name VARCHAR(256) NOT NULL, PRIMARY KEY (id));
