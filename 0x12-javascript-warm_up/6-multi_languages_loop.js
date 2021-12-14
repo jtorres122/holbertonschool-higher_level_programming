a63c3cd83cb0fd347f268ecdc4d7246487b8e942
Julian <jose.torres122@outlook.com>
2021-12-13 22:39:30 -0400

    Script an array of strings with a loop

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const arr = [
+    'C is fun',
+    'Python is cool',
+    'JavaScript is amazing'
+]
+let idx = 0;
+
+for (idx = 0; arr[idx] != null; idx++){
+    console.log(arr[idx]);
+}
