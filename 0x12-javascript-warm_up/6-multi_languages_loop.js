41845e3b847989b0ae2377b2e1daf50b92d16891
Julian <jose.torres122@outlook.com>
2021-12-13 22:40:14 -0400

    Semistandard compliance for Task 6

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 
 const arr = [
-    'C is fun',
-    'Python is cool',
-    'JavaScript is amazing'
-]
+  'C is fun',
+  'Python is cool',
+  'JavaScript is amazing'
+];
 let idx = 0;
 
-for (idx = 0; arr[idx] != null; idx++){
-    console.log(arr[idx]);
+for (idx = 0; arr[idx] != null; idx++) {
+  console.log(arr[idx]);
 }
