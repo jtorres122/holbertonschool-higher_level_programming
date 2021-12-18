31588b00497aa7134c549a50cabf0740ec9e61f9
Julian <jose.torres122@outlook.com>
2021-12-18 18:55:29 -0400

    Fix the issue of incorrect sorting

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,12 +5,12 @@
 import MySQLdb
 from sys import argv
 
-if __name__ == "__main__":
+if __name__ == '__main__':
 
     connector = MySQLdb.connect(host="localhost", port="3306", user=argv[1],
                                 passwd=argv[2], db=argv[3], charset="utf8")
     curs = connector.cursor()
-    curs.execute("SELECT * FROM states ORDER BY id ASC")
+    curs.execute('SELECT * FROM states ORDER BY states.id ASC')
     table = curs.fetchall()
     for row in table:
         print(row)
