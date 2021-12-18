fc986ed59cb88d293036eaded057188753ceb0f5
Julian <jose.torres122@outlook.com>
2021-12-18 19:21:28 -0400

    Too much sql

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -12,8 +12,8 @@ if __name__ == '__main__':
     connector = MySQLdb.connect(host='localhost', port=3306, user=argv[1],
                                 passwd=argv[2], db=argv[3], charset='utf8')
     curs = connector.cursor()
-    curs.execute('SELECT cities.id, cities.name, states.name FROM cities,
-                 states WHERE states.id=state_id ORDER BY cities.id ASC')
+    curs.execute('SELECT cities.id, cities.name, states.name FROM cities,\
+                 states WHERE states.id=state_id')
     table = curs.fetchall()
     for row in table:
         print(row)
