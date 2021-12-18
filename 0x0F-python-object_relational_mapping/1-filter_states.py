baab501dbe28a0c89ffbed3ff0406f3207d39f2d
Julian <jose.torres122@outlook.com>
2021-12-18 19:05:16 -0400

    Add all changes made to task 0 to task 1 to comply with code checkers

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python3
-'''Module lists all the states with the name starting with uppercase N'''
+'''
+Module lists all the states with the name starting with uppercase N
+'''
 
 
 import MySQLdb
 from sys import argv
 
-connector = MySQLdb.connect(host="localhost", port="3306", user=argv[1],
-                            passwd=argv[2], db=argv[3], charset="utf8")
-curs = connector.cursor()
-curs.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
-table = curs.fetchall()
-for row in table:
-    print(row)
-curs.close()
-connector.close()
+if __name__ == '__main__':
+
+    connector = MySQLdb.connect(host='localhost', port=3306, user=argv[1],
+                                passwd=argv[2], db=argv[3], charset='utf8')
+    curs = connector.cursor()
+    curs.execute('SELECT * FROM states WHERE name LIKE "N%" ORDER BY id ASC')
+    table = curs.fetchall()
+    for row in table:
+        print(row)
+    curs.close()
+    connector.close()
