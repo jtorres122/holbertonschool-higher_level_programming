87043be41ca1bf1aad3c6d605ff5e054e651a8f7
Julian <jose.torres122@outlook.com>
2021-12-18 18:57:43 -0400

    Trying to isolate issue causing code checker failure

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-'''Module returns a list of all the states from a database'''
+'''
+Module returns a list of all the states from a database
+'''
 
 
 import MySQLdb
@@ -7,8 +9,8 @@ from sys import argv
 
 if __name__ == '__main__':
 
-    connector = MySQLdb.connect(host="localhost", port="3306", user=argv[1],
-                                passwd=argv[2], db=argv[3], charset="utf8")
+    connector = MySQLdb.connect(host='localhost', port=3306, user=argv[1],
+                                passwd=argv[2], db=argv[3], charset='utf8')
     curs = connector.cursor()
     curs.execute('SELECT * FROM states ORDER BY states.id ASC')
     table = curs.fetchall()
