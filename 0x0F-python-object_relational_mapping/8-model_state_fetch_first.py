53d7dcc10a86fb520b2c01f7d7b10a4088ccc4c6
Julian <jose.torres122@outlook.com>
2021-12-19 13:40:23 -0400

    Be a script that lists first row of State table

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+'''
+Script prints the first State object from the database hbtn_0e_6_usa
+'''
+
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sys import _xoptions, argv
+from model_state import State, Base
+
+if __name__ == '__main__':
+
+    user = argv[1]
+    passwd = argv[2]
+    host = 'localhost'
+    db = argv[3]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'
+                           .format(user, passwd, host, db))
+    session = sessionmaker(bind=engine)
+    Session = session()
+
+    table = Session.query(State).order_by(State.id).first()
+
+    if table is None:
+        print("Nothing")
+    else:
+        print('{}: {}'.format(table.id, table.name))
+
+    Session.close()
