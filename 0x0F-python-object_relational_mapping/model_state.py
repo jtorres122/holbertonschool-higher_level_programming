56e68639428e51f00d8bc9e9bda9ec89a5ae70a6
Julian <jose.torres122@outlook.com>
2021-12-19 10:41:22 -0400

    Be a python file that contains a class definition

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+'''
+Module contains class definition of a State who inherits Base
+'''
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    '''
+    Class adds table states and two fields
+    with defined attributes
+    '''
+
+    __tablename__ = 'states'
+    id = Column(Integer, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
