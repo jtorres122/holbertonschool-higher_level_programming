605cb6053e9299830bfd466973fe8de1009246a0
Julian <jose.torres122@outlook.com>
2021-12-20 13:39:22 -0400

    Clean up my in a rush mess

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-""" Script creates a State with a city in a database"""
+'''Script creates a State with a city in a database'''
+
 
 from relationship_state import Base, State
 from relationship_city import City
@@ -8,13 +9,15 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sys import argv
 
+if __name__ == '__main__':
 
-if __name__ == "__main__":
     engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
                            .format(argv[1], argv[2], argv[3]),
                            pool_pre_ping=True)
+
     Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    session.add(City(name="San Francisco", state=State(name="California")))
-    session.commit()
+    session = sessionmaker(bind=engine)
+    Session = session()
+
+    Session.add(City(name="San Francisco", state=State(name="California")))
+    Session.commit()
--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,23 +1,28 @@
 #!/usr/bin/python3
 '''Script lists all State objects and City objects'''
 
+
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 from relationship_state import State
 from relationship_city import Base, City
 from sys import argv
 
-if __name__ == "__main__":
+if __name__ == '__main__':
+
     engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
                            .format(argv[1], argv[2], argv[3]),
                            pool_pre_ping=True)
     Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    for estado in session.query(State).order_by(State.id):
-        print(estado.id, end="")
-        print(":", estado.name)
-        for city in estado.cities:
+    session = sessionmaker(bind=engine)
+    Session = session()
+
+    table = Session.query(State).order_by(State.id)
+
+    for row in table:
+        print(row.id, end="")
+        print(":", row.name)
+        for city in row.cities:
             print("\t", end="")
             print(city.id, end="")
             print(":", city.name)
