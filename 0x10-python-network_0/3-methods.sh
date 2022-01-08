eac39b30a73f48df4ab45f585c383ea9e5202df0
Julian <jose.torres122@outlook.com>
2022-01-08 17:51:46 -0400

    Display accepted HTTP methods

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script displays all HTTP methods the server will accept
-curl -s -X OPTIONS "$1" -i | grep -i Allow | cut -d ':' -f 2
+curl -s -X OPTIONS "$1" -i | grep "Allow" | cut -d ' ' -f 2-
