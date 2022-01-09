a6abc269c9061579066b2899333b498080590202
Julian <jose.torres122@outlook.com>
2022-01-09 16:20:51 -0400

    Refine but checker is broken

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-'''Module fetches a URL's info'''
+'''Module fetches a URL's data'''
 from urllib import request
 
 
 if __name__ == "__main__":
-    with request.urlopen('https://intranet.hbtn.io/status') as response:
-        info = response.read()
+    with request.urlopen('https://intranet.hbtn.io/status') as resp:
+        data = resp.read()
         print("Body response:")
-        print("    - type: {}".format(type(info)))
-        print("    - content: {}".format(info))
-        print("    - utf8 content: {}".format(info.decode('UTF-8')))
+        print("    - type: {}".format(type(data)))
+        print("    - content: {}".format(data))
+        print("    - utf8 content: {}".format(data.decode("UTF-8")))
