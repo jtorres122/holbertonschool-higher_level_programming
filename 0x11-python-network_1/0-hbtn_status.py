a7433dd3036e757b66fddb9b0c3fdc4729c52759
Julian <jose.torres122@outlook.com>
2022-01-09 12:18:32 -0400

    Try a different method of importing

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 '''Module fetches a URL's info'''
 from urllib import request
-import urllib
 
 
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+    with request.urlopen('https://intranet.hbtn.io/status') as response:
         info = response.read()
         print("Body response:")
         print("    - type: {}".format(type(info)))
         print("    - content: {}".format(info))
-        print("    - utf8 content: {}".format(info.decode('utf8')))
+        print("    - utf8 content: {}".format(info.decode('UTF-8')))
