1a0014018fa1d17869cca8d27543b02da243fc71
Julian <jose.torres122@outlook.com>
2022-01-09 17:47:58 -0400

    Be a script that fetches a URL using get

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -4,7 +4,7 @@ from requests import get
 
 
 if __name__ == "__main__":
-    with get('https://intranet.hbtn.io/status') as resp:
-        print("Body response:")
-        print("    - type: {}".format(type(resp.text)))
-        print("    - content: {}".format(resp.text))
+    resp = get('https://intranet.hbtn.io/status')
+    print("Body response:")
+    print("    - type: {}".format(type(resp.text)))
+    print("    - content: {}".format(resp.text))
