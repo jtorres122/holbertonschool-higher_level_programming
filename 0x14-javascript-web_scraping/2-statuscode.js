007ee6cd3b4dd6fc4ca34c45935dc5250746915c
Julian <jose.torres122@outlook.com>
2022-02-01 20:05:28 -0400

    Add err handling

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,5 +3,9 @@ const request = require('request');
 const url = process.argv[2];
 
 request(url, function (err, response) {
-  console.log('code: ' + response.statusCode);
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('code: ' + response.statusCode);
+  }
 });
